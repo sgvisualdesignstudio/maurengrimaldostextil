a87ff9cc6e41601b93714bd27c57e8d8687c2759
sgvisualdesignstudio <sgvisualdesignstudio@gmail.com>
2024-08-11 15:35:37 -0500

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import {createHashRouter} from 'react-router-dom'
 
-
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <div>Home</div>,
+  },
+  {
+    path: '/about',
+    element: <div>About</div>,
+  }
+  
+])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
